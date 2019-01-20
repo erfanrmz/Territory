cc959549211f596f550f1d2230ccc7f46d28112a
king <erfanrmz@gmail.com>
2019-01-20 16:48:57 +0330

    File created

---
+++ Territory.c
@@ -0,0 +1,26 @@
+//Territory
+//KingRmz
+#include <stdio.h>
+#include <stdlib.h>
+
+//Global Variables
+
+struct satisfaction
+{
+	int people;
+	int court;
+	int treasury;
+};
+char King[30];
+
+
+int main()
+{
+	int menu;
+	printf("Enter your name:\n");
+	scanf("%s",King);
+	printf("1.New Game\n 2.Load Save\n");
+	scanf("%d",&menu);
+
+
+}
