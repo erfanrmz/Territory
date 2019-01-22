5952d6e50a91fdb448b54d0ff74757d46d215a5c
king <erfanrmz@gmail.com>
2019-01-22 04:09:45 +0330

    linked-list Fixed

--- Territory.c
+++ Territory.c
@@ -28,13 +28,25 @@ char King[30];
 
 
 //functions
-struct node *create_node(struct problems info)    // creating new node of list
+void create_node(struct problems info)    // creating new node of list
 {
-	struct node *nn;
-	nn = (struct node *)malloc(sizeof(struct node));
-	nn -> data = info;
-	nn -> next = NULL;
-	return nn;
+	struct node *temp;
+	temp = (struct node *)malloc(sizeof(struct node));
+	temp -> data = info;
+	temp -> next = NULL;
+	if (head == NULL)
+	{
+		head = temp;
+	}
+	else 
+	{
+		struct node *current = head;
+		while (current->next != NULL)
+			current = current -> next;
+		current -> next = temp;
+	}
+	
+	
 } 
 void readproblems()  //read all problems from the files that given(CHOICES.txt and "c*.txt")
 {
@@ -42,7 +54,7 @@ void readproblems()  //read all problems from the files that given(CHOICES.txt a
 	int i = 0;
 	FILE *address,*choice;
 	char choice_address[50];
-	struct node *current = head;
+	struct node **current = &head;
 	struct problems info;
 	char test[200];
 	address = fopen("CHOICES.txt","r+");
@@ -62,8 +74,7 @@ void readproblems()  //read all problems from the files that given(CHOICES.txt a
 		fgets(info.decision2,200,choice);
 		fscanf(choice,"%d%d%d",&info.effect2[0],&info.effect2[1],&info.effect2[2]);
 		info.possibility = 3;
-		current = create_node(info);
-		current = current -> next;
+		create_node(info);
 	}
 	return;
 }
@@ -83,7 +94,8 @@ int main()
 	city.court = 50;
 	city.treasury = 50;
 	readproblems();
-//	printf("%s",head->data.problem);
+//	printf("%d",head->data.possibility);
+	
 
 
 
